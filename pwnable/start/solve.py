7b1c073337cc5a6ae8ee018b0807663b4b2efc0c
tuancvp125 <tuanlaa2k23@cvp.vn>
2024-07-16 18:17:17 +0700

    new

---
+++ pwnable/start/solve.py
@@ -0,0 +1,33 @@
+from pwn import *
+
+def start(argv=[], *a, **kw):
+	if args.GDB:
+		return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+	elif args.REMOTE:
+		return remote(sys.argv[1], sys.argv[2], *a, **kw)
+	else:
+		return process([exe] + argv, *a, **kw)
+
+gdbscript = '''
+init-pwndbg
+b *main
+continue
+'''.format(**locals())
+
+exe = './start'
+
+elf = context.binary = ELF(exe)
+
+sla  = lambda r, s: conn.sendlineafter(r, s)
+sl   = lambda    s: conn.sendline(s)
+sa   = lambda r, s: conn.sendafter(r, s)
+se   = lambda s: conn.send(s)
+ru   = lambda r, **kwargs: conn.recvuntil(r, **kwargs)
+rl   = lambda : conn.recvline()
+uu32 = lambda d: u32(d.ljust(4, b'\x00'))
+uu64 = lambda d: u64(d.ljust(8, b'\x00'))
+
+conn = start()
+
+conn.interactive()
+
